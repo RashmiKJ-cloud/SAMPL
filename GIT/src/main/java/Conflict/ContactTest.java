1c5734a1100dbf762b271dd552df9164910c7417
“Rashmikj” <rashmijagadeesh764@gmail.com>
2021-03-27 10:39:40 +0530

    second commit

--- GIT/src/main/java/Conflict/ContactTest.java
+++ GIT/src/main/java/Conflict/ContactTest.java
@@ -7,18 +7,28 @@ public class ContactTest {
 	@Test
 	public void createcontact() {
 		System.out.println("create testi");
+		
+		System.out.println("testing continuosly");
 	}
 	
 
 	@Test
 	public void Login() {
 		System.out.println("create testin");
+		System.out.println("testing continuosly");
 	}
+	
 	
 
 	@Test
 	public void Test() {
 		System.out.println("create testing");
+	}
+	
+	@Test
+	public void Modify() {
+		//a=10;
+		System.out.println("modification");
 	}
 	
 	
